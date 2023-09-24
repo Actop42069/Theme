2e37d11a580e410e62f6b6cc5346bd8ff0045b67
Achyut <agaihr10@gmail.com>
2023-09-24 15:56:44 +0545

    Header file values changed

--- header.php
+++ header.php
@@ -22,6 +22,8 @@ if ( function_exists( 'wp_body_open' ) ) {
 }
 ?>
 
+
+
 <div id="page" class="site">
 	<header id="masthead" class="site-header" role="banner">
 		<?php get_template_part( 'template-parts/header/nav' ); ?>
