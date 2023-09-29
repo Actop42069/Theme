8a12c714cb6e1bd930eed909986e876618ef1285
Achyut <agaihr10@gmail.com>
2023-09-29 15:26:55 +0545

    Registered sidebars into the singe page

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ * 
+ * @package TechGlazers
+ */
+?>
+
+<aside class="secondary" role="complementary">
+    <?php dynamic_sidebar('sidebar-1'); ?>
+</aside>
--- single.php
+++ single.php
@@ -8,10 +8,13 @@ get_header();
 
 <div id="primary">
   <main id="main" class="site-main mt-5" role="main">
-    <?php
-      if( have_posts() ) :
-        ?>
-        <div class="container">
+   <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 col-sm-12">
+        <?php
+        if( have_posts() ) :
+          ?>
+          <div class="post-wrap">
           <?php
               if ( is_home() && is_front_page() ){
                   ?>
@@ -21,29 +24,24 @@ get_header();
                     </h1>
                   </header>
                   <?php
-              }
-              
-
+              }        
               while ( have_posts() ): the_post();
-
                 get_template_part( 'template-parts/content');
-
                endwhile;
-            ?>
-            </div>
-           
-        </div>
-        <?php
-
-        else : 
-
+               ?>
+               </div>
+      
+          <?php
+          else : 
             get_template_part( 'template-parts/content-none' );
-
-      endif;
-    
-
-
-    ?>
+         endif;
+        ?>
+        </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <?php get_sidebar(); ?>
+        </div>
+      </div>
+   </div>
     <div class="container">
       <?php
            previous_post_link();
